e3ad247ce0ada88b96f71a9afaacf85f878b4018
zus2 <zuscom@gmail.com>
2025-04-27 12:06:19 +0200

    minor

--- dev/multitask_reconnect_remote.py
+++ dev/multitask_reconnect_remote.py
@@ -1,8 +1,12 @@
 # ---
+# multitask_reconnect_remote.py v0.1
 #
 # Reconnect remote if out of range ( or batteries changed .. ) 
 # Runs main() multitask inside a reinitialisation loop
-# 
+# Uses race to stop the multitasking if connection lost
+# You may have to store coroutine local data in globals 
+# Or export if the race condition of deconnection is triggered
+#
 # ---
 
 
@@ -31,10 +35,10 @@ async def my_task_1():
     while True:
         try: 
             remote.name()
-            print('remote connected')
+            print('remote still connected')
             await wait(100)
-        except OSError as ex:
-            print("disconnected")
+        except:
+            print("*** remote disconnected ***")
             connected = False
             break
         await wait(1000)
@@ -51,6 +55,6 @@ while connected == False:
     remote = Remote(timeout=None)
     connected = True
     count += 1
-    print ("instance",count,": connected and running main()")
+    print ("instance",count,": remote connected and running main()")
     run_task(main())
 wait(1000)
